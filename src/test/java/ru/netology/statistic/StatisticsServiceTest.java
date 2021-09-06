de57596623500a0184156f68c8e81165e686d1cb
Liza Degtyareva <liza.degt@yandex.ru>
2021-09-06 22:10:37 +0300

    adding checks

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,8 @@ class StatisticsServiceTest {
   void findMax() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+    long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 10, 11, 1};
+    long expected = 11;
 
     long actual = service.findMax(incomesInBillions);
 
